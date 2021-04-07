e62c7a11c13e826be89278d6fc6b4c9cab7c094f
icaro19 <33903177+icaro19@users.noreply.github.com>
2021-04-07 19:48:55 -0300

    Add files via upload

---
+++ AAAAAAAA/Sem_prod.c
@@ -0,0 +1,214 @@
+/*
+Produtores e consumidores - Semaforo
+gcc Sem_prod.c -o semprod.c -Wall -lpthread -lrt
+
+TODO
+-Definir hierarquia tamanho_buf - prod/cons
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <time.h>
+
+int *buffer;
+int tamanho_buf, itens_prod, itens_consum;
+sem_t mutex;
+sem_t vazio;
+sem_t cheio;
+
+void *produtor();
+void *consumidor();
+int remover_item();
+void consumir_item(int hitem);
+int produzir_item();
+int inserir_item(int hitem);
+
+int main(int argc, char const *argv[])
+{
+	tamanho_buf = atoi(argv[1]);
+	itens_prod = atoi(argv[2]);
+	itens_consum = atoi(argv[3]);
+
+	pthread_t tid_prod, tid_cons;
+	buffer = malloc (tamanho_buf * sizeof(int));
+
+	for (int i = 0; i < tamanho_buf; ++i)
+	{
+		buffer[i] = 0;
+//		printf("%d\n", buffer[i]);
+	}
+
+	sem_init(&mutex, 0, 1);
+	sem_init(&vazio, 0, tamanho_buf);
+	sem_init(&cheio, 0, 0);
+
+
+	if(pthread_create(&tid_prod, NULL, produtor, NULL))
+	{
+		printf("erro ao gerar produtor\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if(pthread_create(&tid_cons, NULL, consumidor, NULL))
+	{
+		printf("erro ao gerar consumidor\n");
+		exit(EXIT_FAILURE);
+	}
+
+	pthread_join(tid_cons, NULL);
+	pthread_join(tid_prod, NULL);
+
+	free(buffer);
+
+	return 0;
+}
+
+//--------------------------------PRODUTOR----------------------------
+
+void *produtor()
+{
+	printf("\n*****producao = %d itens*****\n", itens_prod);
+	int item, status;
+
+	while(1)
+	{
+		
+		for (int i = 0; i < itens_prod && i < tamanho_buf; ++i)
+		{
+			item = produzir_item();
+			
+			sem_wait(&vazio);
+			sem_wait(&mutex);
+
+			status = inserir_item(item);
+
+			if(!status)
+			{
+				printf("\nAviso: buffer cheio\n");
+//				i = itens_prod;
+			}
+
+			else
+			{
+				printf("\nItem %d de %d foi inserido no buffer\n", i+1, itens_prod);
+			}
+
+			sem_post(&mutex);
+			sem_post(&cheio);
+		}
+
+		
+
+		
+
+/*		printf("-----buffer pos-producao:-----\n");
+
+		for (int i = 0; i < tamanho_buf; ++i)
+		{
+			printf("%d ", buffer[i]);
+		}
+
+		printf("\n");*/
+	}
+}
+
+int produzir_item()
+{
+	printf("\nproduzindo 1 item\n");
+	sleep(1);
+
+	return 1;
+}
+
+int inserir_item(int hitem)
+{
+	int i;
+	for (i = 0; buffer[i] != 0 && i < tamanho_buf; ++i);
+
+	if (i < tamanho_buf)
+	{
+		printf("\n---inserindo item no buffer---\n");
+		buffer[i] = 1;
+		sleep(1);
+		return 1;
+
+	}
+
+	return 0;
+	
+}
+
+//-------------------------CONSUMIDOR----------------------
+
+void *consumidor()
+{
+
+	printf("\n*****consumo = %d itens*****\n", itens_consum);
+	int item = 0;
+
+	
+	while(1)
+	{
+
+		for (int i = 0; i < itens_consum && i < tamanho_buf; ++i)
+		{
+			printf("j = %d\n", i);
+			sem_wait(&cheio);
+			sem_wait(&mutex);
+
+			item = remover_item();
+
+			
+			if(!item)
+			{
+				printf("\nAviso: buffer vazio\n");
+			}
+
+			else
+			{
+				consumir_item(item);
+			}
+
+			sem_post(&mutex);
+			sem_post(&vazio);
+		}
+
+
+		
+/*		for (int i = 0; i < tamanho_buf; ++i)
+		{
+			printf("%d \n", buffer[i]);
+		}
+
+		printf("\n");*/
+	}
+
+
+}
+
+
+int remover_item()
+{
+	int i;
+	for (i = 0; i < tamanho_buf && buffer[i] == 0; ++i);
+
+	if (i < tamanho_buf)
+	{
+		printf("\n---removendo item do buffer---\n");
+		sleep(1);
+		buffer[i] = 0;
+		return 1;
+	}
+
+	return 0;
+}
+
+void consumir_item(int hitem)
+{
+	printf("\n---consumindo item do buffer---\n");
+	sleep(1);
+}
---
+++ AAAAAAAA/Thread_Filosofos.c
@@ -0,0 +1,216 @@
+/*
+Filosofos glutoes com threads
+gcc Thread_Filosofos.c -o threadfilo.c -Wall -lpthread
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+
+int num_filosofos, tempo_pensando, tempo_comendo, ESQ, DIR;
+int* estado;
+int* id_filosofo;
+
+//#define ESQ	(*id_filosofo+num_filosofos-1)%num_filosofos
+//#define DIR	(*id_filosofo+1)%num_filosofos
+#define PENSANDO 0
+#define FAMINTO 1
+#define COMENDO 2
+
+void pensar(int indice);
+void *filosofos(void * fil);
+char* lista(int indice);
+void pegar_garfos(int filosofo);
+void soltar_garfos(int filosofo);
+void teste(int indice);
+
+int main (int argc, char *argv[])
+{
+
+	num_filosofos = atoi(argv[1]);
+	tempo_pensando = atoi(argv[2]);
+	tempo_comendo = atoi(argv[3]);
+
+	estado = malloc (num_filosofos * sizeof (int));
+	id_filosofo = malloc (num_filosofos * sizeof (int));
+
+	pthread_t tid[num_filosofos];
+	
+	for (int k = 0; k < num_filosofos; ++k)
+	{
+		id_filosofo[k] = k;
+//		printf("id_filosofo = %d\n", id_filosofo[k]);
+	}
+
+	for (int j = 0; j < num_filosofos; ++j)
+	{
+		pthread_create(&tid[j], NULL, filosofos, (void*)&id_filosofo[j]);
+	}
+
+/*	for (int j = 0; j < num_filosofos; ++j)
+	{
+		pthread_join(tid[j], NULL);
+//		printf("\n---------------------------------\n");
+	}*/
+
+	pthread_exit(NULL);
+
+
+	free(estado);
+	return 0;
+}
+
+void *filosofos(void * fil)
+{
+
+	while (1)
+	{
+//		printf("\n-----filosofos - thread %d-----\n", pthread_self());
+		int i = * (int *) fil;
+		ESQ = (id_filosofo[i]+num_filosofos-1)%num_filosofos;
+		DIR = (id_filosofo[i]+1)%num_filosofos;
+
+//		printf("i = %d\n", i);
+		pensar(i);
+		pegar_garfos(i);
+//		lista(i);
+		soltar_garfos(i);
+	}
+}
+
+void pensar(int indice)
+{
+//	printf("\n-----pensar-----\n");
+	printf("\nFilosofo %d (%s) pensando\n", indice, lista(indice));
+	sleep(tempo_pensando);
+}
+
+
+
+void pegar_garfos(int filosofo)
+{
+//	printf("\n-----pegar_garfos-----\n");
+	estado[filosofo] = FAMINTO;
+	printf("\n-----Filosofo %d (%s) esta faminto-----\n", filosofo, lista(filosofo));
+	teste(filosofo);
+}
+
+void soltar_garfos(int filosofo)
+{
+//	printf("\n-----soltar_garfos-----\n");
+	estado[filosofo] = PENSANDO;
+	printf("\n-----Filosofo %d (%s) soltou os garfos-----\n", filosofo, lista(filosofo));
+//	printf("ESQ = %d\tDir = %d\n", ESQ, DIR);
+	teste(ESQ);
+	teste(DIR);
+}
+
+void teste(int indice)
+{
+
+//	printf("\n-----teste-----\n");
+	if (estado[indice] == FAMINTO && estado[ESQ] != COMENDO && estado[DIR] != COMENDO)
+	{
+		estado[indice] = COMENDO;
+		printf("\n-----Filosofo %d (%s) esta comendo-----\n", indice, lista(indice));
+		sleep(tempo_comendo);
+	}
+
+	/*else
+	{
+		printf("Filosofo %d nao pode lista\n", indice);
+	}*/
+}
+
+char* lista(int indice)
+{
+	switch (indice)
+	{
+		case 1:
+			return "Aristoteles";
+		break;
+
+		case 2:
+			return "Socrates";
+		break;
+
+		case 3:
+			return "Platao";
+		break;
+
+		case 4:
+			return "Tales";
+		break;
+
+		case 5:
+			return "Parmenides";
+		break;
+
+		case 6:
+			return "Democrito";
+		break;
+
+		case 7:
+			return "Pitagoras";
+		break;
+
+		case 8:
+			return "Empedocles";
+		break;
+
+		case 9:
+			return "Xenofanes";
+		break;
+
+		case 10:
+			return "Protagoras";
+		break;
+
+		case 11:
+			return "Gorgias";
+		break;
+
+		case 12:
+			return "Epiteto";
+		break;
+
+		case 13:
+			return "Xenofonte";
+		break;
+
+		case 14:
+			return "Teofrasto";
+		break;
+
+		case 15:
+			return "Alcmeao";
+		break;
+
+		case 16:
+			return "Cleantes";
+		break;
+
+		case 17:
+			return "Apolonio";
+		break;
+
+		case 18:
+			return "Diogenes";
+		break;
+
+		case 19:
+			return "Aristoxenes";
+		break;
+
+		case 20:
+			return "Clearco";
+		break;
+
+		default:
+			return "Cicero";
+	}
+	
+}
